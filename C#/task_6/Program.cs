abdb167d9897923d10f7e686aa5601408981d4a1
kyrylozo <kyzltk@gmail.com>
2023-01-30 16:27:13 +0100

    Task 6 for C#

---
+++ C#/task_6/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая на вход принимает число и выдает, является ли число четным 
+
+Console.Write("Введите число для тестирования на четность ");
+string stringNumber = Console.ReadLine();
+int Number = Convert.ToInt32(stringNumber);
+string Result = "нечетное";
+if(Number % 2 == 0) Result = "четное";
+Console.Write("Число ");
+Console.WriteLine (Result);
+
