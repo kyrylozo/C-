2dd56073b854d52a3f24e5d027eb4d9878bb4fe8
kyrylozo <kyzltk@gmail.com>
2023-01-30 15:31:23 +0100

    Task_4 C#

---
+++ C#/task_3/Program.cs
@@ -0,0 +1,8 @@
+﻿// Программа, принимающая на вход три числа и выдающая максимальное число
+
+int a = 22, b = 3, c = 9;
+int max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+Console.Write("max =  ");
+Console.WriteLine(max);
