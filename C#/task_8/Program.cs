4797ba480c67cbe3b8e988c841428be0c065971d
kyrylozo <kyzltk@gmail.com>
2023-01-31 18:50:55 +0100

    Подсчет всех четных чисел от 1 до 54

---
+++ C#/task_8/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа, принимающая на вход число (N) и показывающая на выходе все четные
+// числа от 1 до N
+
+Console.Write("Введите число N ");
+string NumberN = Console.ReadLine();
+int N = Convert.ToInt32(NumberN);
+int i = 1;
+Console.Write("Все четные числа от 1 до ");
+Console.WriteLine(NumberN);
+while(i < N + 1)
+{
+    if(i % 2 == 0) 
+    {
+        Console.Write(i);
+        Console.Write("; ");
+    }
+i++;
+}
+
+
+
