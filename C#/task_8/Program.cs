b6f4fddd7cb254ea42242bbb92a6a6e8fe4c2851
kyrylozo <kyzltk@gmail.com>
2023-02-02 13:57:17 +0100

    Added new mode for terminal output: "Console.Write($"{variable}; "

--- C#/task_8/Program.cs
+++ C#/task_8/Program.cs
@@ -3,16 +3,19 @@
 
 Console.Write("Введите число N ");
 string NumberN = Console.ReadLine();
-int N = Convert.ToInt32(NumberN);
+//int N = Convert.ToInt32(NumberN); // это - наиболее универсальный вариант
+int N = int.Parse(NumberN); // Этот вариант - только если аргумент - stirng
 int i = 1;
-Console.Write("Все четные числа от 1 до ");
-Console.WriteLine(NumberN);
+Console.Write($"Все четные числа от 1 до {N}: ");
+//Console.WriteLine(NumberN);
 while(i < N + 1)
 {
     if(i % 2 == 0) 
     {
-        Console.Write(i);
-        Console.Write("; ");
+        //Console.Write(i);
+        //Console.Write("; ");
+        Console.Write($"{i}; ");
+        
     }
 i++;
 }
