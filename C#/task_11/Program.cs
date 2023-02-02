9fe2d42c2521fa27ec91c9e9ed4730933b45db1a
kyrylozo <kyzltk@gmail.com>
2023-02-02 21:32:07 +0100

    Программа, которая выводит случайное трехзначеное число и удаляет вторую цифру этого числа

---
+++ C#/task_11/Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа, которая выводит случайное трехзначеное число и удаляет вторую цифру этого числа
+
+int rand = new Random().Next(100, 1000);
+Console.WriteLine($"Initial random number: {rand}  ");
+int number3 = rand % 10;
+int number1 = rand / 100;
+Console.WriteLine($"Number1 = {number1}");
+Console.WriteLine($"Number3 = {number3}");
+int number = number1 * 10 + number3;
+Console.WriteLine($"Worked random number {number}");
+
