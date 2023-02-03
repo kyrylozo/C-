bc9244ccb7d7866b2756d3b67988a294e1bc80e1
kyrylozo <kyzltk@gmail.com>
2023-02-03 04:41:46 +0100

    программа, принимающая на вход два числа и выводящая, является ли второе число кратным первому

---
+++ C#/task_12/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая будет принимать на вход два числа и выводить, 
+// является ли второе число кратным первому.
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+Console.WriteLine("Enter two numbers");
+string stringnumberA = Console.ReadLine();
+int numberA = Convert.ToInt32(stringnumberA);
+string stringnumberB = Console.ReadLine();
+int numberB = Convert.ToInt32(stringnumberB);
+if (numberA % numberB == 0)
+{
+    Console.WriteLine("Кратное");
+}
+    else
+    {
+        int result = numberA % numberB;
+        Console.WriteLine($"Не кратное. Остаток = {result}");
+    }
