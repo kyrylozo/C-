03650d1852154bf3b19995ecb00aa4328aa47936
kyrylozo <kyzltk@gmail.com>
2023-02-03 05:07:35 +0100

    Программа, принимающая на вход число и проверяющая, кратно ли оно одновроеменно 7 и 23

---
+++ C#/task_14/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет, 
+// кратно ли оно одновременно 7 и 23
+
+Console.WriteLine("Enter number");
+string stringnumber = Console.ReadLine();
+int number = Convert.ToInt32(stringnumber);
+
+if (number % 7 > 0)
+{
+        int result7 = number % 7;
+        Console.WriteLine($"Не кратное. Остаток при делении на 7 = {result7}");
+}
+    else if (number % 23 > 0)
+    {
+        int result23 = number % 23;
+        Console.WriteLine($"Не кратное. Остаток при делении на 23 = {result23}");
+    }
+        else
+        {
+            Console.WriteLine("Число кратно 7 и 23");
+        }
+
+
+
