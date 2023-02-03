438e96403d9c0087469acb3633ea83987aac7dfd
kyrylozo <kyzltk@gmail.com>
2023-02-03 12:31:53 +0100

    Выводит третью цифру заданного числа или сообщает,что третьей цифры нет

---
+++ C#/task_13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Выводит третью цифру заданного числа или сообщает,что третьей цифры нет.
+
+Console.Write("Введите число: ");
+string stNum = Console.ReadLine();
+int num = Convert.ToInt32(stNum);
+
+if(num * 0.01 < 1)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+    else
+    {
+        while(num > 999)
+        {
+        num = num / 10;
+        }
+            int num3 = num % 10;
+            Console.WriteLine($"Третья цифра числа = {num3}");
+    }
+
