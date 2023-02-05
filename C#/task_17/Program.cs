d8de0fe4c25d8a48b9ab25beb5bae115f9aa772e
kyrylozo <kyzltk@gmail.com>
2023-02-05 07:29:52 +0100

    Принимает на вход координаты точки (X, Y), причем X не равен 0 и Y не равен 0 и выдит номер четверти плоскости, в которой находится эта точка

---
+++ C#/task_17/Program.cs
@@ -0,0 +1,28 @@
+﻿// Принимает на вход координаты точки (X, Y), причем X не равен 0 и Y не равен 0 и выдает
+// номер четверти плоскости, в которой находится эта точка
+
+Console.Write("Enter number ");
+int numX = int.Parse(Console.ReadLine());
+
+Console.Write("Enter number ");
+int numY = int.Parse(Console.ReadLine());
+
+if (numX > 0 && numY > 0 )
+{
+    Console.WriteLine("1 Section");
+}
+
+else if (numX < 0 && numY > 0 )
+{
+    Console.WriteLine("2 Section");
+}
+
+else if (numX < 0 && numY < 0 )
+{
+    Console.WriteLine("3 Section");
+}
+
+else if (numX > 0 && numY < 0 )
+{
+    Console.WriteLine("4 Section");
+}
