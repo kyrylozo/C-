6a53fdd431f01ff0def181a6d07b0e730fb971af
kyrylozo <kyzltk@gmail.com>
2023-02-06 11:03:21 +0100

    Принимает на вход число (N)  выдает таблицу кубов чисел от 1 до N

---
+++ C#/task_23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Принимает на воход число (N)  выдает таблицу кубов чисел от 1 до N
+
+
+Console.Write("Enter number N: ");
+int N = int.Parse(Console.ReadLine());
+byte ind = 1;
+Console.WriteLine("Кубы чисел от 0 до указанного числа включительно: ");
+
+while (ind <= N)
+{
+    Console.Write($"| {Math.Pow(ind,3)}");
+    ind++;
+}
