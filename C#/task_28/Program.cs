d57a1af8998e36082c6dfec94b7eeaf8afeb8101
kyrylozo <kyzltk@gmail.com>
2023-02-09 14:08:19 +0100

    Принимает на вход число N и выдает произведение чисел от 1 до N

---
+++ C#/task_28/Program.cs
@@ -0,0 +1,24 @@
+﻿// 28. Принимает на вход число N и выдает произведение чисел от 1 до N
+
+Console.Write("Enter the number ");
+sbyte num = sbyte.Parse(Console.ReadLine());
+if (num < 0)
+{
+    Console.WriteLine($"Число должно быть больше 0");
+}
+    else
+        {
+             Console.WriteLine($"Факториал числа N = {Factorial(num)}"); 
+        }
+
+// Method
+double Factorial(sbyte number)
+{
+    double factorial = 1;
+    for (byte index = 1; number >= index; index++)
+    {
+        //factorial = factorial * index;
+        factorial *= index;
+    }
+    return factorial;
+}
