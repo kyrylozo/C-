c93c519d0c43fe8879848237d3c1676424e40338
kyrylozo <kyzltk@gmail.com>
2023-02-09 13:17:28 +0100

    Принимает на вход число и выдает количество цифр в числ

---
+++ C#/task_26/Program.cs
@@ -0,0 +1,17 @@
+﻿// Принимает на вход число и выдает количество цифр в числе
+
+Console.Write("Enter the number ");
+double num = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Количество цифр в числе: {CountFigures(num)}");
+
+// Method
+double CountFigures(double number)
+{
+    byte index = 0;
+    for (; number >= 1; index++)
+    {
+        number = number / 10;
+    }
+    return index;
+}
