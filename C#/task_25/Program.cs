199e5a7a69a6a4237b8359e584f8f4c3d0b25ee0
kyrylozo <kyzltk@gmail.com>
2023-02-12 18:57:56 +0100

    цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B (+ заполнение массива)

--- C#/task_25/Program.cs
+++ C#/task_25/Program.cs
@@ -1,24 +1,22 @@
-﻿// 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A 
-// в натуральную степень B.
+﻿// 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 
 // (методом цикла for, где получаем А и Б (без метода  Math.Pow()))
 
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
-
 // ==============================================================
-
-//sbyte GetAB(sbyte num1, sbyte num2)
-//{
-//    sbyte[] Array = new sbyte[2];
-//    Console.WriteLine("Enter the 1st number: ");
-//    Array[0] = sbyte.Parse(Console.ReadLine());
-//    Console.Write("Enter the 2d number: ");
-//    Array[1] = sbyte.Parse(Console.ReadLine());
-//    return Array;
-//    Console.Write($"Entered numbers {Array[0]}, {Array[1]}");
-//}
+// int[]Get_AB()
+// {
+//     int[] Array = new int[2];
+//     Console.Write("Enter the 1st number: ");
+//     Array[0] = int.Parse(Console.ReadLine());
+//     Console.Write("Enter the 2d number: ");
+//     Array[1] = int.Parse(Console.ReadLine());
+//     return Array;
+    
+// }
+// Console.Write(String.Join(", ", Get_AB()));
 // ==============================================================
 
 Console.Write("Enter the 1st number: ");
