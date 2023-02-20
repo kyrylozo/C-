cbd0d354460f366ce08a53068e186f7a4e9451f2
kyrylozo <kyzltk@gmail.com>
2023-02-20 17:02:55 +0100

    Переворачивает одномерный массив (последний элемент будет на первом месте, а первый - на последнем)

---
+++ C#/039_Massive_Transvertion/Program.cs
@@ -0,0 +1,42 @@
+﻿// 39. Переворачивает одномерный массив (последний элемент будет на первом месте, а первый - на последнем)
+
+int CreateArrayVolume() // Создание случайного объема массива
+{
+    int volume = new Random().Next(3,25);
+    return volume;
+}
+
+int[] CreateArray(int volume) // Создание массива объемом: "Volume"
+{
+    int[] Volume = new int[volume];
+    return Volume;
+}
+
+int[] FillArray(int[] array) // Заполнение массива случайными цифрами от 1 до 9
+{
+    int[] Array = array;
+    int count = Array.Length;
+    Console.WriteLine($"Array volume: {count}");
+    for (byte i = 0; i < count; i++)
+    {
+        Array[i] = new Random().Next(1,10);
+    }
+    return Array;
+}
+
+
+int[] InvertArray(int[] array) // Переворачивавет одномерный массив
+{
+    int temp = 0;
+    for(byte i = 0; i <= (array.Length -1) / 2; i++)
+    {
+        temp = array[i];
+        array[i] = array[((array.Length - 1) - i)];
+        array[((array.Length - 1) - i)] = temp;
+    }
+    return array;
+}
+int[] CreatedArray = FillArray(CreateArray(CreateArrayVolume()));
+Console.WriteLine(String.Join(" ,", CreatedArray));
+
+Console.WriteLine(String.Join(" ,", InvertArray(CreatedArray)));
