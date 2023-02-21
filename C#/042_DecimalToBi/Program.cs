d9cb45061d2f77f35dc1bae72b14c8039bf5f9d6
kyrylozo <kyzltk@gmail.com>
2023-02-21 21:03:07 +0100

    Преобразовывает десятичное число в двоичное

---
+++ C#/042_DecimalToBi/Program.cs
@@ -0,0 +1,19 @@
+﻿// Преобразовывает десятичное число в двоичное
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int[] Numbers(int num)
+{
+    int size = 32;
+    int[] result = new int [size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = num % 2;
+        num /= 2;
+    }
+    return result;
+}
+int[] Binary = (Numbers(number));
+Array.Reverse(Binary);
+Console.WriteLine("Полученный массив = "+ String.Join(" ", Binary));
