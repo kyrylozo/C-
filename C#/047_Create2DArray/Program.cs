22ae7c0ccb379941bf5cb574771f39afb09c40b5
kyrylozo <kyzltk@gmail.com>
2023-03-05 20:17:48 +0100

    Создает двумерный массив размером m x n, заполненный случайными вещественными числами

---
+++ C#/047_Create2DArray/Program.cs
@@ -0,0 +1,27 @@
+﻿// Создает двумерный массив размером m x n, заполненный случайными вещественными числами
+// m = 3, n = 4
+
+// Создание таблицы и наполнение ее случайными числами
+
+void MakeArray(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"_{new Random().Next(1, 10)}|");
+        }
+        System.Console.WriteLine();
+    }
+} 
+
+int[,] matrix = new int[3, 4];
+MakeArray(matrix);
+
+
+
+
+
+
+
+
